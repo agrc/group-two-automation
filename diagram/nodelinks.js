edd4d1f24e517b048139fd2e57dbaa326d090dbf
steveoh <sgourley@utah.gov>
2017-11-29 16:16:31 -0700

    initial

---
+++ diagram/nodelinks.js
@@ -0,0 +1,248 @@
+var relationships = [{
+    source: 'Municipal Boundaries',
+    target: 'Parcels',
+    type: 'alignment'
+}, {
+    source: 'Parcels',
+    target: 'Address Points',
+    type: 'qc'
+}, {
+    source: 'Parcels',
+    target: 'Tax',
+    type: 'qc'
+}, {
+    source: 'Parcels',
+    target: 'PLSS Fabric',
+    type: 'qc'
+}, {
+    source: 'Parcels',
+    target: 'Voting',
+    type: 'alignment'
+}, {
+    source: 'Parcels',
+    target: 'PLSS',
+    type: 'data'
+}, {
+    source: 'Municipal Boundaries',
+    target: 'Address Points',
+    type: 'data'
+}, {
+    source: 'Municipal Boundaries',
+    target: 'Tax',
+    type: 'alignment'
+}, {
+    source: 'Municipal Boundaries',
+    target: 'Political Boundaries',
+    type: 'alignment'
+}, {
+    source: 'Municipal Boundaries',
+    target: 'Enterprize Zones',
+    type: 'alignment'
+}, {
+    source: 'Municipal Boundaries',
+    target: 'E911',
+    type: 'alignment'
+}, {
+    source: 'Address Points',
+    target: 'Geocoding',
+    type: 'data'
+}, {
+    source: 'Address Points',
+    target: 'E911',
+    type: 'data'
+}, {
+    source: 'Address Grids',
+    target: 'Address Points',
+    type: 'data'
+}, {
+    source: 'PLSS Fabric',
+    target: 'Land Ownership',
+    type: 'alignment'
+}, {
+    source: 'PLSS Fabric',
+    target: 'Tax',
+    type: 'alignment'
+}, {
+    source: 'PLSS Fabric',
+    target: 'Municipal Boundaries',
+    type: 'alignment'
+}, {
+    source: 'Roads',
+    target: 'Geocoding',
+    type: 'qc'
+}, {
+    source: 'Roads',
+    target: 'E911',
+    type: 'data'
+}, {
+    source: 'Address Grids',
+    target: 'Roads',
+    type: 'data'
+}, {
+    source: 'Roads',
+    target: 'Road Network',
+    type: 'data'
+}, {
+    source: 'Zip Codes',
+    target: 'Roads',
+    type: 'data'
+}, {
+    source: 'County Boundaries',
+    target: 'Roads',
+    type: 'data'
+}, {
+    source: 'Tax',
+    target: 'Sales Tax',
+    type: 'data'
+},{
+    source: 'Zip Codes',
+    target: 'Address Points',
+    type: 'qc'
+}, {
+    source: 'County Boundaries',
+    target: 'Address Points',
+    type: 'qc'
+}, {
+    source: 'Political Boundaries',
+    target: 'Vista Ballot',
+    type: 'alignment'
+}, {
+    source: 'Political Boundaries',
+    target: 'County Boundaries',
+    type: 'alignment'
+}];
+
+relationships.sort(function(a, b) {
+   if (a.source > b.source) {
+       return 1;
+   }
+   else if (a.source < b.source) {
+       return -1;
+   }
+   else {
+       if (a.target > b.target) {
+           return 1;
+       }
+       if (a.target < b.target) {
+           return -1;
+       }
+       else {
+           return 0;
+       }
+   }
+});
+
+// any relationships with duplicate source and target get an incremented 'linknum'
+for (var i=0; i<relationships.length; i++) {
+    if (i != 0 &&
+        relationships[i].source == relationships[i-1].source &&
+        relationships[i].target == relationships[i-1].target) {
+            relationships[i].linknum = relationships[i-1].linknum + 1;
+        }
+    else {
+        relationships[i].linknum = 1;
+    }
+};
+
+var nodes = {};
+
+// Compute the distinct nodes from the relationships.
+relationships.forEach(function(link) {
+  link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+  link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+});
+
+var w = 1900,
+    h = 800;
+
+var force = d3.layout.force()
+    .nodes(d3.values(nodes))
+    .links(relationships)
+    .size([w, h])
+    .linkDistance(125)
+    .charge(-3000)
+    .on('tick', tick)
+    .start();
+
+var svg = d3.select('body').append('svg:svg')
+    .attr('width', w)
+    .attr('height', h);
+
+    svg.append('svg:defs').selectAll('marker')
+        .data(['alignment', 'data', 'qc'])
+      .enter().append('svg:marker')
+        .attr('id', String)
+        .attr('viewBox', '0 -5 10 10')
+        .attr('refX', 63)
+        .attr('refY', -3.5)
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 6)
+        .attr('orient', 'auto')
+
+      .append('svg:path')
+        .attr('d', 'M0,-5L10,0L0,5');
+    var path = svg.append('svg:g').selectAll('path')
+        .data(['alignment', 'data', 'qc'])
+      .enter().append('svg:path')
+        .attr('class', function(d) { return 'lixnk ' + d; })
+        .attr('marker-end', function(d) { return 'url(#' + d + ')'; });
+
+// Per-type markers, as they don't inherit styles.
+svg.append('svg:defs').selectAll('marker')
+    .data(['alignment', 'data', 'qc'])
+  .enter().append('svg:marker')
+    .attr('id', String)
+    .attr('viewBox', '0 -5 10 10')
+    .attr('refX', 63)
+    .attr('refY', -3.5)
+    .attr('markerWidth', 6)
+    .attr('markerHeight', 6)
+    .attr('orient', 'auto')
+  .append('svg:path')
+    .attr('d', 'M0,-5L10,0L0,5');
+
+var path = svg.append('svg:g').selectAll('path')
+    .data(force.links())
+  .enter().append('svg:path')
+    .attr('class', function(d) { return 'link ' + d.type; })
+    .attr('marker-end', function(d) { return 'url(#' + d.type + ')'; });
+
+var circle = svg.append('svg:g').selectAll('circle')
+    .data(force.nodes())
+  .enter().append('svg:circle')
+    .attr('r', 10)
+    .call(force.drag);
+
+var text = svg.append('svg:g').selectAll('g')
+    .data(force.nodes())
+  .enter().append('svg:g');
+
+// A copy of the text with a thick white stroke for legibility.
+text.append('svg:text')
+    .attr('x', 15)
+    .attr('y', '.31em')
+    .attr('class', 'shadow')
+    .text(function(d) { return d.name; });
+
+text.append('svg:text')
+    .attr('x', 15)
+    .attr('y', '.31em')
+    .text(function(d) { return d.name; });
+
+// Use elliptical arc path segments to doubly-encode directionality.
+function tick() {
+  path.attr('d', function(d) {
+    var dx = d.target.x - d.source.x,
+        dy = d.target.y - d.source.y,
+        dr = 500/d.linknum;  //linknum is defined above
+    return 'M' + d.source.x + ',' + d.source.y + 'A' + dr + ',' + dr + ' 0 0,1 ' + d.target.x + ',' + d.target.y;
+  });
+
+  circle.attr('transform', function(d) {
+    return 'translate(' + d.x + ',' + d.y + ')';
+  });
+
+  text.attr('transform', function(d) {
+    return 'translate(' + d.x + ',' + d.y + ')';
+  });
+}
---
+++ readme.md
@@ -0,0 +1,143 @@
+## Identify all internal SGID-related data processes
+
+##### Address Points
+ - Zach
+ - Greg
+
+##### Broadband
+ - Zach
+ - Bert
+
+##### Boundaries
+ - Mike
+ - **Datasets**
+  - Municipalities
+  - County
+  - Voting/Precinct
+  - Tax
+
+##### Roads
+ - Michael
+ - Greg
+
+##### Parcels
+ - Rick
+ - **Datasets**
+    - Normal Parcels
+    - LIR
+
+##### Imagery
+ - Rick
+ - **Datasets**
+    - Indices
+    - Raster.utah.gov
+
+##### Zone Boundaries
+ - Dave
+ - **Datasets**
+    - PSAPS
+    - Fire
+    - EMS
+    - Law
+    - City Codes
+
+## Document and review processes
+1. Address Points
+  1. Existing scripts are written, 1 per participating county
+  1. Incomplete because data is not received from
+    - Daggett
+    - Unitah
+    - San Juan
+    - Grand
+    - Garfield
+    - Sanpete
+1. Broadband
+  1. needs documentation but automation is not the best fit
+1. Boundaries
+  1. Municipal/County Boundaries
+    1. PDF or jpg provied by LGO after county submission
+    2. Outline is created with COGO tools in state plane and added to muni_modifications
+    3. (muni/county)_modification polygon is **manually** used to adjust official boundary feature class and muni carto feature class
+    4. blog post created quarterly
+  1. Precints (Vista Ballot Areas)
+    1. Mostly shapefiles received from counties
+    2. ETL **manually** into our schema
+    3. Validated on overlap with political districts and issues fixed
+  1. PLSS Fabric
+    1. BLM appends mini fabrics
+    2. Counties and surveyors can submit tie sheets for corner adjustments
+    3. Uses mostly built in fabric tools from vendor
+    4. Exported to PLSS GCDB format and appends attributes **manually**
+  1. Tax Nomenclature
+    1. Terri C from tax provides individual geodatabases
+    2. **Manually** create new tax areas using tax [process doc](https://docs.google.com/document/d/1Z8i0icMP3EeV0sI2BENhZNfAVTbfuyZfSm59Mwb3XBg/edit)
+    3. **Manual** and scripted review happens based on checklist with exceptions
+    3. Create prior years and publish new years
+    4. Data can be updated quarterly
+    5. Blog post is created yearly
+1. Roads
+  1. Data is requested from counties based on a schedule
+  1. Data is edited in UTRANS
+  2. Scripts are run to ETL data
+  3. Change detection is run
+  4. Custom tool helps reconcile differences in provided vs new data
+  5. Monthly blog post
+1. Parcels
+  1. Existing scripts ETL data into our schema. One per county.
+1. Sales Tax Areas need to align with municipal and county boundaries
+  1. schema and attribute checks
+1. Health stuff is loaded/edited adhoc when reached out to by stewards
+  1. email sent with instructions
+1. PSAPS
+  1. Only certain dispatch centers are supported
+    1. box elder
+    1. vernal dispatch
+      1. Daggett, Duchesne, Uintah
+    1. salt lake communication center (TOC)
+    1. weber/morgan
+    1. cedar dispatch
+      2. Iron co
+      1. cedar also dispatches DPS on highways, etc
+    1. st george dispatch
+    1. san juan
+    1. beaver
+    1. miller
+  1. Address Points
+    1. ETL address points **manually**
+      1. Sevier, Wayne, and Piute Counties they send David the address points directly
+    1. geocodes address points and inspects less than 100% matches
+    1. fixes issues **manually**
+  1. Roads
+    1. Inspectes segments with geocodes scores < 100%
+    2. Edits in spillman and Utrans with a note
+1. Schools
+  1. Scrape data from office of ed
+  2. ETL to our schema **manually**
+  3. Validate addresses and possibly move point
+1. EMS Response Zones
+  1. Not sure where they come from or how they are maintained.
+1. Care Centers
+  1. Get from Health some how
+  1. Addresses are validated **manually**
+1. Enterprise Zones
+  1. Kelleigh sends shapefiles in emails
+  2. Align boundaries with muni boundaries **manually** splitting where they cross
+1. Zip Codes
+  1. Align boundary with street segments **manually**
+  2. Updated after talking to cities and jurisdictions
+  3. Can change when code gets subdivided
+1. Address Grids
+  1. Align to boundaries **manually**
+
+#### Decide which processes are best to automate
+
+1. David is requesting help with dispatch data sets
+
+#### Plan an implementation for automatable processes
+
+#### Determine data dependencies
+
+![graph](./data-relationships.png)
+1. green dash = geometry/attributes are derived
+2. red dash = used for quality control
+3. black = data aligns to geometries
